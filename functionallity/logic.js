de24a238c346b491910ea514658442ef95525801
Emmanuel Vega <carlito_sky@hotmail.com>
2021-04-16 01:16:50 -0500

    Winner desition-done, MachinePlayer-done, cleanBoard-missed

--- functionallity/logic.js
+++ functionallity/logic.js
@@ -30,39 +30,43 @@ class Board {
 }
 
 function mainLoop(turn, activateFlipper) {
-    opponentTeam = turn == 'player'?'blacks':'whites';
-    teamPlayer = turn == 'player'?'whites':'blacks';
-    console.log(`Es turno es de ${masterBoard.T} del team ${teamPlayer}`); 
+    opponentColorTeam = turn == 'player'?'blacks':'whites';
+    colorTeam = turn == 'player'?'whites':'blacks';
+    console.log(`Es turno es de ${masterBoard.T} del team ${colorTeam}`); 
     for (var tokens in masterBoard){
         if(masterBoard[tokens] != masterBoard.T){
-            if(masterBoard[tokens] && masterBoard[tokens].colorGroup == teamPlayer){
+            if(masterBoard[tokens] && masterBoard[tokens].colorGroup == colorTeam){
                 neighborChecker(masterBoard[tokens].coords,activateFlipper);                                
             }
         }
     }
 }
 
-function tokenFlipper(tokens, opponentTeam){
-            for(var token of tokens){
+function tokenFlipper(tokens, opponentColorTeam){
+        for(var token of tokens){
             selectors[token].innerHTML = `
-            <div class="token ${opponentTeam}" ></div> 
+            <div class="token ${opponentColorTeam}" ></div> 
             `;
-            masterBoard[token].colorGroup = teamPlayer;
+            masterBoard[token].colorGroup = colorTeam;
         }  
 }
 
 function whoWon () {
-    var teamPlayerTokens=0;
-    var opponentTeamTokens=0;
+    var colorTeamTokens=0;
+    var opponentColorTeamTokens=0;
     for (var token in masterBoard){
-        if((masterBoard[token] != masterBoard.T) && (masterBoard[token])){
-            if (masterBoard[token].colorGroup === teamPlayer){
-                teamPlayerTokens++;
-            }else opponentTeamTokens++;
-        const winTeam = teamPlayerTokens > opponentTeamTokens ? 'player' : 'machine';
-        alert(`EL GANADOR ES ${winTeam} CON ${teamPlayerTokens} FICHAS`);
+        if(masterBoard[token] != masterBoard.T){
+            if(masterBoard[token] && masterBoard[token].colorGroup == 'whites'){
+                colorTeamTokens++;
+                console.log(`Voy en el token ${token} y van ${colorTeamTokens} del team whites`);
+            }else if(masterBoard[token] && masterBoard[token].colorGroup == 'blacks'){
+                opponentColorTeamTokens++;
+                console.log(`Voy en el token ${token} y van ${opponentColorTeamTokens} del team blacks`);
+            }
         }
     }
+    const winTeam = colorTeamTokens > opponentColorTeamTokens ? ['whites',colorTeamTokens] : ['blacks',opponentColorTeamTokens];
+    alert(`GANARON LAS FICHAS ${(winTeam[0]).toUpperCase()} CON ${winTeam[1]} FICHAS`);
 }
 
 function tokenBuilder(){
@@ -70,28 +74,47 @@ function tokenBuilder(){
         A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8
     }
     coords = [values[(this.id).charAt(0)],parseInt((this.id).charAt(1))];
-    masterBoard[this.id] = new Token(this.id,teamPlayer,coords);
+    masterBoard[this.id] = new Token(this.id,colorTeam,coords);
     masterBoard[this.id].dropper();
-
     neighborChecker(coords, true);
     masterBoard.T = masterBoard.T === 'player'?'machine':'player';
     tokensFlipped = false;
     mainLoop(masterBoard.T, false);
     if (activeListeners == 0){
-        whoWon();
-        return;
+        if (activeListeners.length == 0){
+            for (var token in masterBoard){
+                masterBoard[token];
+            }
+            console.log(masterBoard);
+        }
+            setTimeout(() => {
+                whoWon();   
+            }, 500);
+            return 
+    }   
+    if (masterBoard.T === 'machine') {
+        setTimeout(() => {
+            machinePlayer(); // calls machine to play when it's its turn    
+        }, 500); 
     }
+}
+
+function machinePlayer() {
+        const opponentToken = Math.floor((activeListeners.length)*Math.random());
+        console.log(opponentToken);
+        console.log(activeListeners[opponentToken]);
+        selectors[activeListeners[opponentToken]].click();
 }
 
 function clickListenerFactory(positionsAvailable){
     selectors[positionsAvailable].addEventListener("click", tokenBuilder);
-    selectors[positionsAvailable].style.backgroundColor = 'white';
+    // selectors[positionsAvailable].style.backgroundColor = 'white';
 }
 
 function clickEventDeleter(positionsAvailable){
     for (var position of positionsAvailable){
         selectors[position].removeEventListener('click', tokenBuilder);
-        selectors[position].style.backgroundColor = 'green';
+        // selectors[position].style.backgroundColor = 'green';
     }
 } 
 
@@ -111,7 +134,7 @@ function neighborChecker(home, activateFlipper) {
 
     for (var neighbor of NEIGHBORS){
         if(masterBoard[neighbor]){
-            if (masterBoard[neighbor].colorGroup == opponentTeam) {
+            if (masterBoard[neighbor].colorGroup == opponentColorTeam) {
                 var newNeighbor ;                   //BEFORE WHILE TO AVOID RE-INICIALIZATION 
                 var track = [neighbor];
                 var direction = NEIGHBORS.indexOf(neighbor);
@@ -148,23 +171,20 @@ function neighborChecker(home, activateFlipper) {
                         break;
                     }
                     if (masterBoard[newNeighbor]) {
-                        if (masterBoard[newNeighbor].colorGroup == opponentTeam) {
+                        if (masterBoard[newNeighbor].colorGroup == opponentColorTeam) {
                             track.push(newNeighbor); // TOKENS THAN SHOULD BE FLIPPED
                             newHome = [
                                 values[newNeighbor.charAt(0)],
                                 parseInt(newNeighbor.charAt(1)),
                               ];
-                        } else if ((masterBoard[newNeighbor].colorGroup == teamPlayer)&&(activateFlipper==true)) {
+                        } else if ((masterBoard[newNeighbor].colorGroup == colorTeam)&&(activateFlipper==true)) {
                             console.log(`FLIPPER EN ${track}`); //APPLY FLIPPER FUNCTION
                             clickEventDeleter(activeListeners);
                             activeListeners = []; 
-                            // setTimeout(  ()=>{
-                                 tokenFlipper(track, teamPlayer);
-                            // },500);
+                            tokenFlipper(track, colorTeam);
                             follower = false;
                             tokensFlipped = true;
-                        }else break;
-                        
+                        }else break;                       
                     } else{
                         if(tokensFlipped == true) return;//IF THE TOKENS WERE FLIPPED IN THE SAME PLAYED, DON'T MAKE LISTENERS OF 'EM
                         else{
@@ -177,7 +197,7 @@ function neighborChecker(home, activateFlipper) {
                                 parseInt(newNeighbor.charAt(1)) < 1
                                 ) break;
                                 else{
-                                // console.log(`PON LA FICHA EN ${newNeighbor}`);
+                                console.log(`PON LA FICHA EN ${newNeighbor}`);
                                 activeListeners.push(newNeighbor);
                                 clickListenerFactory(newNeighbor); //ADDING EVENT LISTENERS
                                 follower = false;
@@ -193,8 +213,8 @@ function neighborChecker(home, activateFlipper) {
 var coords = [];
 var masterBoard = new Board;
 masterBoard.T = 'player';
-var opponentTeam;
-var teamPlayer;
+var opponentColorTeam;
+var colorTeam;
 var winner = [];
 var activeListeners = [];
 var tokensFlipped = false;
